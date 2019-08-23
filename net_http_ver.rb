3548dfe72db757cd17d8b7682cfd67a09186c731
yoyoyoheyheyhey <asokottedotti@icloud.com>
2019-08-24 06:56:17 +0900

    add: net/httpで言語一覧取得クライアント

---
+++ net_http_ver.rb
@@ -0,0 +1,33 @@
+
+require 'net/http'
+require 'uri'
+require 'json'
+require 'csv'
+
+def get_language(id, pass)
+  uri = URI.parse("https://robot.diveintocode.jp:17777/programLanguages")
+  http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = uri.scheme === "https"
+
+  req = Net::HTTP::Get.new(uri.path)
+  req.basic_auth id, pass
+  response = http.request(req)
+  
+  JSON.parse(response.body)
+end
+   
+def output_language(data_format, hash)
+  case data_format
+  when "json"
+    json_str = JSON.pretty_generate(hash)
+    puts json_str
+  when "csv"
+    csv_str = CSV.generate do |csv|
+      csv << hash[0].keys
+      hash.each { |hash| csv << hash.values}
+    end
+    puts csv_str
+  end
+end
+
+output_language(ARGV[0], get_language(ARGV[1], ARGV[2]))
